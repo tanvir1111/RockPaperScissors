4c9958581d7d0ed2f54d023e1b0b0cb47b1308c6
tanvir1111 <53863352+tanvir1111@users.noreply.github.com>
2020-12-11 12:47:59 +0600

    updates

--- app/src/main/java/com/ravinta/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/ravinta/rockpaperscissors/MainActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +20,8 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView rock,paper,scissors,result;
+    ImageView welcomeImg;
+    Animation zoomIn,zoomOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,40 +31,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         paper=findViewById(R.id.paperBtn);
         scissors=findViewById(R.id.scissorsBtn);
         result=findViewById(R.id.result);
+        welcomeImg=findViewById(R.id.welcomeText);
         rock.setOnClickListener(this);
         paper.setOnClickListener(this);
         scissors.setOnClickListener(this);
         result.setOnClickListener(this);
+         zoomIn= AnimationUtils.loadAnimation(this,R.anim.zoom_in);
+        zoomOut= AnimationUtils.loadAnimation(this,R.anim.zoom_out);
+        welcomeImg.setAnimation(zoomIn);
 
 
     }
+
 
     @Override
-    public void onClick(View v) {
+    protected void onResume() {
+        super.onResume();
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
 
-        switch (v.getId()){
-            case R.id.rockBtn:
-                rock.setBackground(v.getBackground());
-
-                showResult("rock");
-                break;
-            case R.id.paperBtn:
-                rock.setBackground(v.getBackground());
-                showResult("paper");
-                break;
-            case R.id.scissorsBtn:
-                rock.setBackground(v.getBackground());
-                showResult("scissors");
-                break;
-            case R.id.result:
+                welcomeImg.setAnimation(zoomOut);
+                welcomeImg.setVisibility(View.GONE);
                 startNew();
-                break;
-        }
+
+
+            }
+        },2000);
+
 
 
 
     }
+    private void startNew() {
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                welcomeImg.setVisibility(View.GONE);
+                result.setVisibility(View.GONE);
+                rock.setBackground(getDrawable(R.drawable.rock));
+                paper.setBackground(getDrawable(R.drawable.paper));
+                scissors.setBackground(getDrawable(R.drawable.scissors));
+                rock.setVisibility(View.VISIBLE);
+                paper.setVisibility(View.VISIBLE);
+                scissors.setVisibility(View.VISIBLE);
 
+
+            }
+        },400);
+
+
+
+
+    }
     void showResult(String userChoice){
         paper.setVisibility(View.GONE);
         String botOptions[]={"rock","paper","scissors"};
@@ -111,16 +138,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    private void startNew() {
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.rockBtn:
+                rock.setBackground(v.getBackground());
+
+                showResult("rock");
+                break;
+            case R.id.paperBtn:
+                rock.setBackground(v.getBackground());
+                showResult("paper");
+                break;
+            case R.id.scissorsBtn:
+                rock.setBackground(v.getBackground());
+                showResult("scissors");
+                break;
+            case R.id.result:
+                startNew();
+                break;
         }
-        finally {
-            Intent intent=new Intent(MainActivity.this,MainActivity.class);
-            startActivity(intent);
-            finish();
-        }
+
+
+
     }
+
+
+
+
 }
